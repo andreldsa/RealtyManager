be13394313fa1f582006e7916af395e569b3f6b0
Andre Luiz dos Santos Abrantes <andredossantosabrantes@gmail.com>
2014-11-11 11:16:17 +0000

    changing pages and before_filter to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  before_filter :require_login
+  
+  def require_login
+    unless current_user
+      redirect_to signin_path, :notice => "You must Log-in!"
+    end
+  end
 end
